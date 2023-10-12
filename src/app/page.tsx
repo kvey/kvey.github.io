0937dddbd0ebbe1d704da63068a7918e6d65773b
coltonpierson <colton@coltonpierson.com>
2023-10-12 11:31:10 -0700

    fix errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import { LineBreak, LineBreakDouble } from '@/components/linebreak';
 import { Logo } from '@/components/logo';
 
-function formatDate(date) {
+function formatDate(date: Date) {
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
