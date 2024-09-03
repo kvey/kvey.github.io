dda6b42e9ffac42ee444a75e233800966942a9fe
Colton Pierson <colton@coltonpierson.com>
2024-09-03 14:44:28 -0700

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import type { AppProps } from 'next/app'
 import Nav from '@/components/nav';
 import {berkeleyMono} from "@/components/font";
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: 'Colton Pierson',
